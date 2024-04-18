a2de354cc1af5c54ab9e8ac3d3d0368f03c632b1
vaibhavgupta <vaibhavg7005@gmail.com>
2024-04-18 13:05:09 +0530

    Lifecycle safe communication

--- app/src/main/java/com/example/android_lifecycle/ConfigChangeFragment.kt
+++ app/src/main/java/com/example/android_lifecycle/ConfigChangeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class ConfigChangeFragment : Fragment() {
+
+    var btnSubmit: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +19,26 @@ class ConfigChangeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_config_change, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnSubmit = view.findViewById(R.id.buttonSubmit)
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        btnSubmit?.setOnClickListener {
+            if(activity is DataSubmittedCallback) {
+                activity.onDataSubmitted()
+            }
+        }
     }
 
     companion object {
         fun newInstance() =
             ConfigChangeFragment()
+    }
+
+    interface DataSubmittedCallback {
+        fun onDataSubmitted()
     }
 }
--- app/src/main/java/com/example/android_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/android_lifecycle/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.example.android_lifecycle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConfigChangeFragment.DataSubmittedCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
         if (savedInstanceState == null) {
             val fragment = ConfigChangeFragment.newInstance()
             supportFragmentManager.beginTransaction()
                 .add(R.id.frameLayout, fragment, "fdf")
                 .commit()
         }
+    }
+
+    override fun onDataSubmitted() {
+        Toast.makeText(this, "Data updated on fragment", Toast.LENGTH_SHORT).show()
     }
 }
